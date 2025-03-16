11dbc01b5ccf832204cf01535d435a66ffc1e735
tomcr <tom.cremer@student.hepl.be>
2025-03-16 12:30:21 +0100

    Update, delete features

--- app/Livewire/Track.php
+++ app/Livewire/Track.php
@@ -4,16 +4,16 @@ namespace App\Livewire;
 
 use Carbon\Carbon;
 use Flux\Flux;
-use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Log;
 use Livewire\Component;
 
 class Track extends Component
 {
     public $selectedDate;
-public $title;
-public $started_at;
-public $ended_at;
+    public $trackId;
+    public $title;
+    public $started_at;
+    public $ended_at;
+
     public function mount()
     {
         $this->selectedDate = Carbon::today()->toDateString(); // Stocker en format 'Y-m-d'
@@ -24,9 +24,15 @@ public $ended_at;
         $this->selectedDate = Carbon::parse($this->selectedDate)->addDays($modifier)->toDateString();
     }
 
+    public function addTrack()
+    {
+        $this->title = '';
+        $this->started_at = '';
+        $this->ended_at = '';
+        Flux::modal('add-track')->show();
+    }
     public function saveTrack()
     {
-
         \App\Models\Track::create([
             'user_id' => auth()->id(),
             'title' => $this->title,
@@ -37,6 +43,38 @@ public $ended_at;
 
         $this->render();
     }
+
+    public function editTrack($trackId)
+    {
+        $track = \App\Models\Track::find($trackId);
+        $this->title = $track->title;
+        $this->started_at = $track->started_at->format('H:i');
+        $this->ended_at = $track->ended_at->format('H:i');
+        $this->trackId = $trackId;
+        Flux::modal('edit-track')->show();
+        $this->render();
+    }
+
+    public function updateTrack($trackId)
+    {
+        $track = \App\Models\Track::find($trackId);
+        $track->update([
+            'title' => $this->title,
+            'started_at' => Carbon::parse($this->selectedDate . ' ' . $this->started_at),
+            'ended_at' => Carbon::parse($this->selectedDate . ' ' . $this->ended_at),
+        ]);
+        Flux::modal('edit-track')->close();
+        $this->trackId = null;
+        $this->render();
+    }
+
+    public function deleteTrack($trackId)
+    {
+        \App\Models\Track::destroy($trackId);
+        $this->trackId = null;
+        $this->render();
+    }
+
     public function render()
     {
         $tracks = \App\Models\Track::where('user_id', auth()->id())->
