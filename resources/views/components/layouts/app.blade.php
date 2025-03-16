4eae05a1a4437c4c437446ac96caffab4edd0519
tomcr <tom.cremer@student.hepl.be>
2025-03-16 12:27:04 +0100

    Refactored the layout to fit my requirements

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,5 +1,14 @@
-<x-layouts.app.sidebar :title="$title ?? null">
-    <flux:main>
-        {{ $slot }}
-    </flux:main>
-</x-layouts.app.sidebar>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark min-h-screen">
+<head>
+    @include('partials.head')
+</head>
+<body class="min-h-screen bg-white dark:bg-zinc-800 grid grid-cols-[auto_1fr] grid-rows-1" style="overflow-x: hidden;">
+@livewire('sidebar')
+<main class="p-5 overflow-y-auto h-screen ">
+    {{ $slot }}
+</main>
+
+@fluxScripts
+</body>
+</html>
