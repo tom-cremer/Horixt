0c0444c1d1276c615bbfb0315b4de6029b27cf97
tomcr <tom.cremer@student.hepl.be>
2025-04-09 00:18:09 +0200

    New Table - Colors

---
+++ database/migrations/0001_01_01_000003_create_colors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('colors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('alias');
+            $table->string('light_color')->nullable();
+            $table->string('dark_color')->nullable();
+            $table->string('text_light_color')->nullable();
+            $table->string('text_dark_color')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('colors');
+    }
+};
