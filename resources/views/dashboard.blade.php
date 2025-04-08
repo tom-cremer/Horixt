c15d16deb070b366ca3662ca67ae747c508f0fb4
tomcr <tom.cremer@student.hepl.be>
2025-04-09 00:25:15 +0200

    Testing the colors

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,3 @@
-<x-layouts.app>
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
         <div class="grid auto-rows-min gap-4 md:grid-cols-3">
             <flux:breadcrumbs>
@@ -6,8 +5,13 @@
             </flux:breadcrumbs>
 
         </div>
-        <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-            <x-placeholder-pattern class="relative size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+
+        <div class="flex flex-wrap gap-2 relative overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
+            @foreach($colors as $color)
+                <div class="inline-block  p-2 w-56 text-center rounded-xl {{$color->alias}} {{$color->alias}}--text flex items-center justify-center ">
+                    {{ $color->name }}
+                </div>
+            @endforeach
+            {{--<x-placeholder-pattern class="relative size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />--}}
         </div>
     </div>
-</x-layouts.app>
