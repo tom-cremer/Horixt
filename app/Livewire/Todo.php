0ecc3548c2f59f08451cba2d8ee2490b2e92d35d
tomcr <tom.cremer@student.hepl.be>
2025-04-17 00:08:53 +0200

    Organization condition added

--- app/Livewire/Todo.php
+++ app/Livewire/Todo.php
@@ -2,11 +2,11 @@
 
 namespace App\Livewire;
 
+use App\Helper\Context;
 use App\Models\Color;
 use App\Models\Priority;
 use App\Models\Status;
 use App\Models\Todo as Todos;
-use Flux\Flux;
 use Livewire\Component;
 
 class Todo extends Component
@@ -23,22 +23,40 @@ class Todo extends Component
 
     public $completedTodos = [];
 
-
     public $priorities;
     public $statuses;
     public $colors;
+
+
     public function createTodo()
     {
-        Todos::create([
-            'name' => $this->name,
-            'description' => $this->description,
-            'is_done' => false,
-            'status_id' => $this->status_id ?? Status::DEFAULT,
-            'priority_id' => $this->priority ?? Priority::DEFAULT,
-            'color_id' => $this->color_id ?? Color::DEFAULT,
-            'user_id' => auth()->id(),
-            'project_id' => $this->project_id ?? null,
-        ]);
+        if (Context::isOrganization()) {
+
+            Todos::create([
+                'name' => $this->name,
+                'description' => $this->description,
+                'is_done' => false,
+                'status_id' => $this->status_id ?? Status::DEFAULT,
+                'priority_id' => $this->priority ?? Priority::DEFAULT,
+                'color_id' => $this->color_id ?? Color::DEFAULT,
+                'user_id' => auth()->id(),
+                'project_id' => $this->project_id ?? null,
+                'organization_id' => Context::getOrganizationId(),
+            ]);
+
+        } else {
+            Todos::create([
+                'name' => $this->name,
+                'description' => $this->description,
+                'is_done' => false,
+                'status_id' => $this->status_id ?? Status::DEFAULT,
+                'priority_id' => $this->priority ?? Priority::DEFAULT,
+                'color_id' => $this->color_id ?? Color::DEFAULT,
+                'user_id' => auth()->id(),
+                'project_id' => $this->project_id ?? null,
+                'organization_id' => null,
+            ]);
+        }
         $this->reset();
     }
 
@@ -56,8 +74,15 @@ class Todo extends Component
         $this->priorities = Priority::all();
         $this->statuses = Status::all();
         $this->colors = Color::all();
-        $todos = Todos::where('user_id', auth()->id())->with(['project', 'status', 'priority'])->get();
-        $this->completedTodos = Todos::where('user_id', auth()->id())->where('is_done', true)->get()->pluck('id')->toArray();
+
+        if (Context::isOrganization()) {
+            $todos = Todos::where('user_id', auth()->id())->where('organization_id', Context::getOrganizationId())->with(['project', 'status', 'priority'])->get();
+            $this->completedTodos = Todos::where('user_id', auth()->id())->where('is_done', true)->where('organization_id', Context::getOrganizationId())->with(['project', 'status', 'priority'])->get();
+        } else {
+            $todos = Todos::where('user_id', auth()->id())->where('organization_id', null)->with(['project', 'status', 'priority'])->get();
+            $this->completedTodos = Todos::where('user_id', auth()->id())->where('is_done', true)->where('organization_id')->with(['project', 'status', 'priority'])->get();
+        }
+
         return view('livewire.todo', [
             'todos' => $todos,
             'priorities' => $this->priorities,
