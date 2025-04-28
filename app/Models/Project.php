aacdfe5a76fe5b675b57c1a1cf6121d623e016e8
tomcr <tom.cremer@student.hepl.be>
2025-04-29 01:58:57 +0200

    Added Organization_id

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -14,6 +14,7 @@ class Project extends Model
         'name',
         'description',
         'user_id',
+        'organization_id',
         'status_id',
         'priority_id',
         'color_id',
