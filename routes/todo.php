0b373b0a38a396c2c2d9e321bdca34deb2ae5a9f
tomcr <tom.cremer@student.hepl.be>
2025-04-29 01:59:26 +0200

    No usage anymore

--- routes/todo.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-use App\Livewire\Todo;
-use Illuminate\Support\Facades\Route;
-
-Route::middleware(['auth'])->group(function () {
-    Route::get('/todos', Todo::class)->name('todos');
-});
--- routes/track.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Livewire\Track;
-use Illuminate\Support\Facades\Route;
-
-Route::middleware(['auth'])->group(function () {
-    Route::get('/tracks', Track::class)->name('tracks');
-
-});
