d5f6f7abcedee5aa6f2e596c02bb75cde6e27aad
tomcr <tom.cremer@student.hepl.be>
2025-04-29 02:10:49 +0200

    Slight changes

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,6 @@ use Livewire\Volt\Volt;
 
 //Imports
 require __DIR__ . '/auth.php';
-require __DIR__ . '/track.php';
-require __DIR__ . '/project.php';
-require __DIR__ . '/todo.php';
 
 // Principal views
 
@@ -20,24 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-
-
-/*Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
-
-    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
-    Volt::route('settings/password', 'settings.password')->name('settings.password');
-    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
-});*/
-/*
- * Route prefix /organisation -> group
- *      route prefix /{id} -> group middleware('organization')
- *          Route get /settings to component Settings
- *
- *
- * Route prefix /personal -> group middleware('personal')
- *          Route get /settings to component Settings
- * */
 
 Route::prefix('/personal')
     ->middleware(['personal'])
@@ -49,10 +28,9 @@ Route::prefix('/personal')
         Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
 
         Route::get('/dashboard', Dashboard::class)->name('dashboard');
-        Route::get('/tracks', Track::class)->name('tracks');
         Route::get('/todos', Todo::class)->name('todos');
         Route::get('/projects', ProjectList::class)->name('projects.index');
-        Route::get('/projects/{id}', ProjectDetails::class)->name('projects.show');
+        Route::get('/projects/{projectid}', ProjectDetails::class)->name('projects.show');
 
     });
 
@@ -68,7 +46,6 @@ Route::prefix('/organization/{id}')
 
         Route::get('/dashboard', Dashboard::class)
             ->name('dashboard');
-        Route::get('/tracks', Track::class)->name('tracks');
         Route::get('/todos', Todo::class)->name('todos');
         Route::get('/projects', ProjectList::class)->name('projects.index');
         Route::get('/projects/{projectid}', ProjectDetails::class)->name('projects.show');
