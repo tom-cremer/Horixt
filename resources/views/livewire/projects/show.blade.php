5132d393e91d1dd4a46deae7f7a72f2b2cc6a161
tomcr <tom.cremer@student.hepl.be>
2025-04-29 02:03:52 +0200

    Reorganized the Html > better UI

--- resources/views/livewire/projects/show.blade.php
+++ resources/views/livewire/projects/show.blade.php
@@ -1,5 +1,9 @@
 <div>
+    <div class="flex items-center mb-4">
+        <flux:button href="{{ \App\Helper\Context::isOrganization() ? route('organization.projects.index', \App\Helper\Context::getOrganizationId()): route('personal.projects.index') }}">
+            Retour
+        </flux:button>
+    </div>
     <h2 class="text-xl font-bold">{{ $project->name }}</h2>
     <p>{{ $project->description }}</p>
-    <a href="{{ route('projects.index') }}" wire:navigate class="mt-2 bg-gray-500 text-white p-2 rounded">Retour</a>
 </div>
