5d071e0647518c9a1b46f68ea1def36777ca4a7d
tomcr <tom.cremer@student.hepl.be>
2025-05-02 19:24:40 +0200

    Uuid added for future update

--- database/migrations/2025_04_13_101953_create_organizations_table.php
+++ database/migrations/2025_04_13_101953_create_organizations_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('organizations', function (Blueprint $table) {
             $table->id();
+            $table->uuid('uuid')->unique();
             $table->string('name');
             $table->string('slug')->unique();
             $table->text('description')->nullable();
