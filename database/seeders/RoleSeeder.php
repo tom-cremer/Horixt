8ea363a32543027257590265ddd14224d4e57b74
tomcr <tom.cremer@student.hepl.be>
2025-05-08 02:19:55 +0200

    Added Permission to more roles

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -15,8 +15,59 @@ class RoleSeeder extends Seeder
 
         $roles = [
             RoleEnum::ADMIN->value => Permission::all()->pluck('name')->toArray(), // Admin gets all permissions
+            RoleEnum::MEMBER ->value => [
+                PermissionEnum::FILES_VIEW->value,
+                PermissionEnum::FILES_UPLOAD->value,
+                PermissionEnum::FILES_RENAME->value,
+                PermissionEnum::FILES_DOWNLOAD->value,
+                PermissionEnum::FILES_SHARE->value,
+                PermissionEnum::FILES_DELETE->value,
+                PermissionEnum::FILES_MOVE->value,
+                PermissionEnum::FILES_LOCK->value,
+                PermissionEnum::FILES_UNLOCK->value,
+                PermissionEnum::TODOS_VIEW->value,
+                PermissionEnum::TODOS_CREATE->value,
+                PermissionEnum::TODOS_UPDATE->value,
+                PermissionEnum::TODOS_DELETE->value,
+                PermissionEnum::TODOS_ASSIGN->value,
+                PermissionEnum::TRACKS_VIEW->value,
+                PermissionEnum::TRACKS_CREATE->value,
+                PermissionEnum::TRACKS_UPDATE->value,
+                PermissionEnum::TRACKS_DELETE->value,
+                PermissionEnum::PROJECT_VIEW->value,
+                PermissionEnum::PROJECT_CREATE->value,
+            ],
 
+            RoleEnum::REVIEWER->value => [
+                PermissionEnum::TODOS_COMMENT->value,
+                PermissionEnum::TODOS_REVIEW->value,
+            ],
 
+            RoleEnum::INTERN->value => [
+                PermissionEnum::FILES_VIEW->value,
+                PermissionEnum::FILES_UPLOAD->value,
+                PermissionEnum::FILES_RENAME->value,
+                PermissionEnum::FILES_DOWNLOAD->value,
+                PermissionEnum::FILES_MOVE->value,
+                PermissionEnum::TODOS_VIEW->value,
+                PermissionEnum::TODOS_CREATE->value,
+                PermissionEnum::TODOS_UPDATE->value,
+                PermissionEnum::TODOS_DELETE->value,
+                PermissionEnum::TODOS_ASSIGN->value,
+                PermissionEnum::TRACKS_VIEW->value,
+                PermissionEnum::TRACKS_CREATE->value,
+                PermissionEnum::TRACKS_UPDATE->value,
+                PermissionEnum::TRACKS_DELETE->value,
+                PermissionEnum::PROJECT_VIEW->value,
+                PermissionEnum::PROJECT_CREATE->value,
+            ],
+
+            RoleEnum::GUEST->value => [
+                PermissionEnum::FILES_VIEW->value,
+                PermissionEnum::TODOS_VIEW->value,
+                PermissionEnum::TRACKS_VIEW->value,
+                PermissionEnum::PROJECT_VIEW->value,
+            ],
         ];
 
         foreach ($roles as $roleName => $permissions) {
