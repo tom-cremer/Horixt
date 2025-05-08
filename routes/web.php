98cb96390a633c78b2abe8023705df0307e867a8
tomcr <tom.cremer@student.hepl.be>
2025-05-08 02:25:11 +0200

    Added the 2 new routes for Organizations & Member tabs

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,11 @@
 
 use App\Livewire\Dashboard;
 use App\Livewire\FileManager;
+use App\Livewire\Members;
+use App\Livewire\Organizations;
 use App\Livewire\ProjectDetails;
 use App\Livewire\ProjectList;
 use App\Livewire\Todo;
-use App\Livewire\Track;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
@@ -22,13 +23,14 @@ Route::get('/', function () {
 Route::prefix('/personal')
     ->middleware(['personal'])
     ->name('personal.')
-    ->group(function () {
+    ->group(callback: function () {
         Route::redirect('settings', 'settings/profile')->name('settings');
         Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
         Volt::route('settings/password', 'settings.password')->name('settings.password');
         Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
 
         Route::get('/dashboard', Dashboard::class)->name('dashboard');
+        Route::get('/organizations', Organizations::class)->name('organizations');
         Route::get('/todos', Todo::class)->name('todos');
         Route::get('/projects', ProjectList::class)->name('projects.index');
         Route::get('/projects/{projectid}', ProjectDetails::class)->name('projects.show');
@@ -46,8 +48,8 @@ Route::prefix('/organization/{slug}')
         Volt::route('settings/password', 'settings.password')->name('settings.password');
         Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
 
-        Route::get('/dashboard', Dashboard::class)
-            ->name('dashboard');
+        Route::get('/dashboard', Dashboard::class)->name('dashboard');
+        Route::get('/members', Members::class)->name('members');
         Route::get('/todos', Todo::class)->name('todos');
         Route::get('/projects', ProjectList::class)->name('projects.index');
         Route::get('/projects/{projectid}', ProjectDetails::class)->name('projects.show');
