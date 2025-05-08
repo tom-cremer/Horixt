2a69bbfab39576bd983c928ccff849459c9a3ce0
tomcr <tom.cremer@student.hepl.be>
2025-05-08 02:24:26 +0200

    When creating a user, it also creates his or her personal Folder

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -12,7 +12,7 @@ class UserObserver
      */
     public function created(User $user): void
     {
-        FileManagerHelper::createUserDirectory($user->uuid);
+        FileManagerHelper::createUserDirectory($user);
     }
 
     /**
