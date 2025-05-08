adee34b3b7d02262f6b9f6f1f27209dcf01f8979
tomcr <tom.cremer@student.hepl.be>
2025-05-08 02:10:13 +0200

    Files Table migration

--- database/migrations/2025_05_05_090340_create_files_table.php
+++ database/migrations/2025_05_05_090340_create_files_table.php
@@ -13,6 +13,20 @@ return new class extends Migration
     {
         Schema::create('files', function (Blueprint $table) {
             $table->id();
+            $table->string('name'); // Name of the file
+            $table->string('alt')->nullable(); // Alt text for the file
+            $table->string('path'); // Path to the file
+            $table->integer('size'); // Size in bytes
+            $table->string('extension'); // File extension
+            $table->string('disk'); // Storage disk (local, s3, etc.)
+            $table->string('mime_type'); // MIME type of the file
+            $table->string('visibility'); // Visibility (public, private, etc.)
+            $table->string('checksum')->nullable();
+            $table->boolean('locked')->default(false); // Whether the file is locked or not, by the user(s)
+            $table->unsignedBigInteger('user_id'); // User who uploaded the file
+            $table->unsignedBigInteger('organization_id')->nullable(); // Organization ID if applicable
+            $table->unsignedBigInteger('project_id')->nullable(); // Project ID if applicable
+            $table->unsignedBigInteger('directory_id')->nullable(); // Folder ID if applicable
             $table->timestamps();
         });
     }
