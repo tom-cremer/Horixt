0021dad981cf3af84690aa08dd6f0f7e22aaaa10
tomcr <tom.cremer@student.hepl.be>
2025-05-11 01:17:56 +0200

    Slight Changes in the style + Tooltip

--- resources/views/livewire/partials/team-bubble.blade.php
+++ resources/views/livewire/partials/team-bubble.blade.php
@@ -3,28 +3,30 @@
     @if($mode === 'members')
 
         @foreach($members->take(5) as $member)
-            <div
-                class="w-8 h-8 rounded-full border-1 border-white flex items-center justify-center ">
-            <span class="text-xs font-bold font-lexend">
-                {{$member->initials()}}
-            </span>
-            </div>
+            <flux:tooltip content="{{$member->name}}" position="bottom">
+                <div
+                    class="peer w-8 h-8 rounded-full border-1 bg-zinc-600 border-white flex items-center justify-center -m-0.5">
+                    <span class="text-xs font-bold font-lexend ">
+                        {{$member->initials()}}
+                    </span>
+                </div>
+            </flux:tooltip>
         @endforeach
         @if(count($members) >= 5)
-            <div
-                class="w-8 h-8 rounded-full border-1 border-white flex items-center justify-center bg-gray-200">
-        <span class="text-xs font-bold font-lexend">
-            +{{ count($members) - 5 }}
-        </span>
+            <div class="w-8 h-8 rounded-full border-1 border-white flex items-center justify-center bg-gray-200">
+                <span class="text-xs font-bold font-lexend">
+                    +{{ count($members) - 5 }}
+                </span>
             </div>
         @endif
 
     @else
-        <div
-            class="w-8 h-8 rounded-full border-1 border-white flex items-center justify-center ">
-            <span class="text-xs font-bold font-lexend">
-                {{$owner->initials()}}
-            </span>
-        </div>
+        <flux:tooltip content="{{$owner->name}}" position="bottom">
+            <div class="w-8 h-8 rounded-full border-1 border-white flex items-center justify-center ">
+                <span class="text-xs font-bold font-lexend">
+                    {{$owner->initials()}}
+                </span>
+            </div>
+        </flux:tooltip>
     @endif
 </div>
