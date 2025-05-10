3eef460311b6ed4c2382d8e4fab32006446763ae
tomcr <tom.cremer@student.hepl.be>
2025-05-11 01:27:17 +0200

    Organization Invite component w/token

---
+++ app/Livewire/OrgInvite.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Enums\RoleEnum;
+use App\Models\OrganizationInvites;
+use App\Models\User;
+use Livewire\Attributes\Computed;
+use Livewire\Component;
+
+class OrgInvite extends Component
+{
+
+    public $token;
+
+    #[Computed]
+    public $invite;
+    public $organization;
+    public $invited_by;
+    public $invited_user;
+
+
+    public function mount($token)
+    {
+        $this->token = $token;
+        $this->invite = OrganizationInvites::where('token', $this->token)->first();
+        $this->organization = $this->invite->organization;
+        $this->invited_by = $this->invite->user;
+        $this->invited_user = User::where('email', $this->invite->email)->first();
+
+    }
+
+    public function accept()
+    {
+        // Logic to accept the invite
+        if ($this->invited_user) {
+            $this->invite->status = 'accepted';
+            $this->invite->save();
+
+            $this->invited_user->organizations()->attach($this->organization->id, [
+                'joined_at' => now(),
+            ]);
+            // Set Member Role to Invited_user
+            session(['team_id' => $this->organization->id]);
+            setPermissionsTeamId(session('team_id'));
+            auth()->user()->assignRole(RoleEnum::MEMBER->value);
+        }
+    }
+
+    public function createAccount()
+    {
+        return redirect()->route('register', ['email' => $this->invite->email]);
+    }
+
+    public function render()
+    {
+        return view('livewire.org-invite')->layout('components.layouts.invite-layout');
+    }
+}
