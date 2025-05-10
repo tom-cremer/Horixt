365ed200c01535fb1a021d65c1a5f24ec28113d7
tomcr <tom.cremer@student.hepl.be>
2025-05-11 01:14:42 +0200

    New interface for the members.blade.php view

--- app/Livewire/Members.php
+++ app/Livewire/Members.php
@@ -4,6 +4,8 @@ namespace App\Livewire;
 
 use App\Helper\Context;
 use App\Models\Organization;
+use Flux\Concerns\InteractsWithComponents;
+use Flux\Flux;
 use Livewire\Component;
 
 class Members extends Component
@@ -15,7 +17,6 @@ class Members extends Component
     {
         $this->members = Organization::find(Context::getOrganizationId())->members()->withPivot('is_active')->get();
     }
-
 
     public function render()
     {
--- resources/views/livewire/members.blade.php
+++ resources/views/livewire/members.blade.php
@@ -1,45 +1,63 @@
 <div class="overflow-hidden">
     <div class="flex justify-between items-center mb-4">
         <h2 class="text-xl font-semibold">Members</h2>
-        <input
-            wire:model.debounce.300ms="search"
-            type="text"
-            placeholder="Search members..."
-            class="border border-gray-300 rounded px-3 py-1"
-        >
+        <livewire:partials.add-members/>
     </div>
 
+
     <div class="overflow-x-auto">
-        <table class="min-w-full bg-white dark:bg-zinc-600 rounded-lg shadow overflow-auto">
-            <thead class="bg-gray-100 dark:bg-zinc-600 text-left text-sm font-medium text-gray-700">
-            <tr class="">
-                <th class="px-4 py-3 text-black dark:text-white rounded-lg ">Name</th>
+        <table class="min-w-full shadow overflow-auto border border-zinc-300 dark:border-zinc-600 ">
+            <thead class=" text-left text-sm font-medium text-gray-700">
+            <tr class="border-b border-zinc-300 dark:border-zinc-600">
+                <th class="px-4 py-3 text-black dark:text-white  ">Name</th>
                 <th class="px-4 py-3 text-black dark:text-white">Email</th>
                 <th class="px-4 py-3 text-black dark:text-white">Role</th>
                 <th class="px-4 py-3 text-black dark:text-white">Status</th>
                 <th class="px-4 py-3 text-black dark:text-white">Joined</th>
-                <th class="px-4 py-3 text-black dark:text-white text-right rounded-lg">Actions</th>
+                <th class="px-4 py-3 text-black dark:text-white text-right">Actions</th>
             </tr>
             </thead>
             <tbody class="divide-y divide-gray-200 text-sm">
             @forelse ($members as $member)
-                <tr>
-                    <td class="px-4 py-3 flex items-center gap-2">
-                        <img src="" alt="{{ $member->name }}" class="w-8 h-8 rounded-full">
-                        <span>{{ $member->name }}</span>
+                <tr class="border-b border-zinc-300 dark:border-zinc-600">
+                    <td class=" px-4 py-3  border-r border-zinc-300 dark:border-zinc-600">
+                        <div class="flex flex-nowrap items-center gap-2">
+
+                            @if ($member->profile_photo_path)
+                                <img src="{{ $member->profile_photo_url }}" alt="{{ $member->name }}"
+                                     class="w-8 h-8 rounded-full">
+                            @else
+                                <div class="min-w-8 min-h-8 rounded-full bg-gray-200 flex items-center justify-center">
+                                    <span class="text-gray-500 text-sm">{{ $member->initials()}}</span>
+                                </div>
+                            @endif
+
+                            <span>{{ $member->name }}</span>
+                        </div>
                     </td>
-                    <td class="px-4 py-3">{{ $member->email }}</td>
-                    <td class="px-4 py-3">
-                        <flux:badge color="amber" size="sm">{{ $member->roles->first()?->name ?? '' }}</flux:badge>
+                    <td class="px-4 py-3 border-r border-zinc-300 dark:border-zinc-600">
+                        {{ $member->email }}
+                    </td>
+                    <td class="px-4 py-3 border-r border-zinc-300 dark:border-zinc-600">
+                        <div class="flex items-center gap-2">
+                            @if ($member->isOwner(\App\Helper\Context::getOrganizationId()))
+                                <flux:badge color="green" size="sm">Owner</flux:badge>
+                            @endif
+                            @forelse($member->roles as $role)
+                                <flux:badge color="{{\App\Enums\RoleEnum::from($role->name)->color()}}"
+                                            size="sm">{{ ucfirst($role->name) }}</flux:badge>
+                            @empty
+                            @endforelse
+                        </div>
                     </td>
 
-                    <td class="px-4 py-3">
+                    <td class="px-4 py-3 border-r border-zinc-300 dark:border-zinc-600">
                         {{ $member->pivot->is_active ? 'Active' : 'Inactive' }}
                     </td>
-                    <td class="px-4 py-3">
+                    <td class="px-4 py-3 border-r border-zinc-300 dark:border-zinc-600 text-nowrap">
                         {{ $member->pivot->created_at->locale('en_US')->diffForHumans() }}
                     </td>
-                    <td class="px-4 py-3 text-right">
+                    <td class="px-4 py-3 text-right ">
                         <div class="flex justify-end gap-2">
                             <button wire:click="edit({{ $member->id }})"
                                     class="text-blue-600 hover:underline text-sm">Edit
