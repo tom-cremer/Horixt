a3be80c828253f443d89710463f65d443c51fb71
tomcr <tom.cremer@student.hepl.be>
2025-05-17 00:26:04 +0200

    Added a Features tabs system

--- app/Livewire/ProjectDetails.php
+++ app/Livewire/ProjectDetails.php
@@ -2,23 +2,52 @@
 
 namespace App\Livewire;
 
+use App\Livewire\Component\HorixtComponent;
 use App\Models\Project;
 use Livewire\Component;
 
-class ProjectDetails extends Component
+class ProjectDetails extends HorixtComponent
 {
 
     public Project $project;
+
+    public $features = [
+        'todos' => [
+            'label' => 'Todos',
+            'icon' => 'list-todo',
+        ],
+        'files' => [
+            'label' => 'Files',
+            'icon' => 'folder',
+        ],
+        'insights' => [
+            'label' => 'Insights',
+            'icon' => 'chart-spline',
+        ],
+        'settings' => [
+            'label' => 'Settings',
+            'icon' => 'settings',
+        ],
+    ];
+
+
+    public $activeFeature = 'todos';
 
     public function mount($projectid)
     {
         $this->project = Project::find($projectid);
     }
 
+    public function changeView(string $key)
+    {
+        $this->activeFeature = $key;
+    }
+
+
     public function render()
     {
 
-        return view('livewire.projects.show' , [
+        return view('livewire.projects.show', [
             'project' => $this->project
         ]);
     }
--- resources/views/livewire/projects/show.blade.php
+++ resources/views/livewire/projects/show.blade.php
@@ -1,9 +1,19 @@
-<div>
-    <div class="flex items-center mb-4">
-        <flux:button href="{{ \App\Helper\Context::isOrganization() ? route('organization.projects.index', \App\Helper\Context::getOrganizationSlug()): route('personal.projects.index') }}">
-            Retour
-        </flux:button>
+<div class="h-full flex flex-col overflow-clip">
+
+    <flux:navbar>
+        @foreach($features as $key => $feature)
+            <flux:navbar.item wire:click="changeView('{{$key}}')" icon="{{$feature['icon']}}" :current="($activeFeature === $key)" >{{ucfirst($feature['label'])}}</flux:navbar.item>
+        @endforeach
+    </flux:navbar>
+    <div class="py-3 flex-1 overflow-hidden">
+        @if($activeFeature === 'todos')
+            <livewire:todo :projectId="$project->id"/>
+        @elseif($activeFeature === 'files')
+            Files
+        @elseif($activeFeature === 'insights')
+            Insights
+        @elseif($activeFeature === 'settings')
+            Settings
+        @endif
     </div>
-    <h2 class="text-xl font-bold">{{ $project->name }}</h2>
-    <p>{{ $project->description }}</p>
 </div>
