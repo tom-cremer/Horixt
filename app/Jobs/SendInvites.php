6edcb1e3c0fef22a8218232d9636669ba00f946e
tomcr <tom.cremer@student.hepl.be>
2025-05-17 00:32:31 +0200

    Reworked the query of the existing invites

--- app/Jobs/SendInvites.php
+++ app/Jobs/SendInvites.php
@@ -2,28 +2,26 @@
 
 namespace App\Jobs;
 
-use App\Helper\Context;
 use App\Mail\InviteEmail;
 use App\Models\Organization;
 use App\Models\OrganizationInvites;
 use App\Models\User;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
 class SendInvites implements ShouldQueue
 {
     use Queueable;
 
-    public array $inviteesList;
+    public $inviteesList;
     public User $user;
     public Organization $organization;
 
     /**
      * Create a new job instance.
      */
-    public function __construct(array $inviteesList, User $user, Organization $organization)
+    public function __construct($inviteesList, User $user, Organization $organization)
     {
         $this->inviteesList = $inviteesList;
         $this->user = $user;
@@ -36,13 +34,12 @@ class SendInvites implements ShouldQueue
      */
     public function handle(): void
     {
-        Log::info('Sending invites to: ' . implode(', ', $this->inviteesList));
         // Logic to send invites
         foreach ($this->inviteesList as $invitee) {
             $token = md5($invitee . time());
 
             // Check if the invitee already exists in the organization
-            $existingUser = OrganizationInvites::where('email', $invitee)->first();
+            $existingUser = OrganizationInvites::where('email', $invitee)->where('status', 'like', 'pending')->where('created_at', '>=', now()->subDay(7))->first();
             if ($existingUser) {
                 // If the invitee already exists, skip sending the invite
                 continue;
