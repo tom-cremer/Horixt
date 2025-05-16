76b887f6f4b247cdf6340e8bd58245bbffb57914
tomcr <tom.cremer@student.hepl.be>
2025-05-17 00:27:04 +0200

    Removed the Creation of the Organization

--- app/Livewire/Header.php
+++ app/Livewire/Header.php
@@ -3,44 +3,20 @@
 namespace App\Livewire;
 
 use App\Enums\RoleEnum;
+use App\Livewire\Component\HorixtComponent;
 use App\Models\Organization;
 use Flux\Flux;
 use Livewire\Component;
 
-class Header extends Component
+class Header extends HorixtComponent
 {
-    public $name;
-    public $description;
-    public $website;
-    public $email;
-    public $phone;
-    public $logo;
+
     public $owner_id;
     public $slug;
 
     public function mount()
     {
         $this->owner_id = auth()->user()->id;
-    }
-
-    public function createOrganization()
-    {
-        $this->slug = str($this->name)->slug();
-
-        Organization::create([
-            'name' => $this->name,
-            'description' => $this->description,
-            'website' => $this->website,
-            'email' => $this->email,
-            'phone' => $this->phone,
-            'logo' => $this->logo,
-            'owner_id' => $this->owner_id,
-            'slug' => $this->slug,
-        ]);
-
-        Flux::modal('create-organization')->close();
-
-        $this->dispatch('organization-created');
     }
 
     public function render()
--- resources/views/livewire/header.blade.php
+++ resources/views/livewire/header.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <flux:header class="flex justify-end gap-5">
+    <flux:header class="flex justify-end gap-5 px-0!">
         <livewire:partials.breadcrumb/>
         <div class="max-w-[500px] w-full">
             <flux:input
@@ -8,70 +8,6 @@
                 placeholder="{{ __('Search') }}"
             />
         </div>
-        @if (\App\Helper\Context::isPersonal())
-            <flux:modal.trigger name="create-organization">
-                <flux:button
-                    variant="primary"
-                    aria-label="{{ __('Create new') }}"
-                >
-                    Create Organization
-                </flux:button>
-            </flux:modal.trigger>
-        @endif
+        <livewire:partials.tracks.active-tracks/>
     </flux:header>
-
-    <flux:modal name="create-organization">
-        <div class="space-y-6">
-            <div>
-                <flux:heading size="lg">Create Organization</flux:heading>
-                <flux:subheading>Fill in the details below to create a new organization.</flux:subheading>
-            </div>
-
-            <flux:input
-                wire:model="name"
-                type="text"
-                placeholder="{{ __('Organization Name') }}"
-                required
-            />
-
-            <flux:input
-                wire:model="description"
-                type="textarea"
-                placeholder="{{ __('Description') }}"
-                required
-            />
-
-            <flux:input
-                wire:model="email"
-                type="email"
-                placeholder="{{ __('Email') }}"
-                required
-            />
-
-            <flux:input
-                wire:model="website"
-                type="url"
-                placeholder="{{ __('Website') }}"
-                required
-            />
-
-            <flux:input
-                wire:model="phone"
-                type="tel"
-                placeholder="{{ __('Phone') }}"
-                required
-            />
-
-            <div class="flex">
-                <flux:spacer/>
-                <flux:button
-                    variant="primary"
-                    type="submit"
-                    wire:click="createOrganization">
-                    {{ __('Create') }}
-                </flux:button>
-            </div>
-        </div>
-    </flux:modal>
-
 </div>
