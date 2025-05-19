7a42c37f3d448d25d588a36810fdaf2f08eaa459
tomcr <tom.cremer@student.hepl.be>
2025-05-19 02:09:15 +0200

    Added - Creation of Folder + Upload of files + preview for image

--- app/Livewire/FileManager.php
+++ app/Livewire/FileManager.php
@@ -4,20 +4,36 @@ namespace App\Livewire;
 
 use App\Livewire\Component\HorixtComponent;
 use App\Models\Directories;
-use Illuminate\Support\Facades\Log;
-use Livewire\Component;
+use App\Models\Files;
+use Flux\Flux;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Livewire\Attributes\Validate;
+use Livewire\WithFileUploads;
 
 class FileManager extends HorixtComponent
 {
+    use WithFileUploads;
+
     public ?Directories $currentDirectory = null;
     public $breadcrumbs = [];
+
+    #[Validate('max:10240')] /*Max file size 10MB*/
+    public $upload;
+
+    /*Modal*/
+    public bool $contextMenu = false;
+    public $x = 0;
+    public $y = 0;
+
+    public $selectedFile;
 
     public function mount()
     {
         $this->currentDirectory = auth()->user()->directories()
             ->where('path', 'like', '%personal/' . auth()->user()->uuid . '%')
             ->where('user_id', auth()->id())
-            ->where('parent_id', null)
+            ->whereNull('parent_id')
             ->first();
         $this->buildBreadcrumbs();
     }
@@ -45,15 +61,101 @@ class FileManager extends HorixtComponent
         $this->breadcrumbs = array_reverse($breadcrumbs);
     }
 
+
     public function createDirectory()
     {
+        $name = 'New Folder';
 
+        // Génère un nom unique si un dossier du même nom existe déjà
+        $existing = $this->currentDirectory->children()
+            ->where('name', 'LIKE', "{$name}%")
+            ->count();
+
+        if ($existing > 0) {
+            $name .= " ({$existing})";
+        }
+
+        $path = $this->currentDirectory->path . '/' . Str::slug($name);
+
+        // Crée le dossier physiquement
+        Storage::disk('local')->makeDirectory("{$path}");
+
+        // Enregistre en BDD
+        $directory = Directories::create([
+            'name' => $name,
+            'path' => $path,
+            'disk' => 'local',
+            'visibility' => 'private',
+            'locked' => false,
+            'protected' => false,
+            'user_id' => auth()->id(),
+            'organization_id' => $this->currentDirectory->organization_id,
+            'project_id' => $this->currentDirectory->project_id,
+            'parent_id' => $this->currentDirectory->id,
+        ]);
+
+
+    }
+
+    public function fileUploadModal()
+    {
+        Flux::modal('upload-file')->show();
+    }
+
+    public function uploadFile()
+    {
+
+        $file = $this->upload;
+
+        $storagePath = $this->currentDirectory->path;
+        $fileName = $file->getClientOriginalName();
+
+        $storedPath = $file->storeAs(
+            "{$storagePath}",
+            $fileName
+        );
+
+        Files::create([
+            'name' => pathinfo($fileName, PATHINFO_FILENAME),
+            'alt' => null,
+            'path' => $storagePath . '/' . $fileName,
+            'size' => $file->getSize(),
+            'extension' => $file->getClientOriginalExtension(),
+            'disk' => 'local',
+            'mime_type' => $file->getMimeType(),
+            'visibility' => 'private',
+            'checksum' => null, // tu peux ajouter plus tard
+            'locked' => false,
+            'user_id' => auth()->id(),
+            'organization_id' => $this->currentDirectory->organization_id,
+            'project_id' => $this->currentDirectory->project_id,
+            'directory_id' => $this->currentDirectory->id,
+        ]);
+
+        $this->reset('upload');
+        Flux::modal('upload-file')->close();
+    }
+
+    public function download()
+    {
+        return Storage::disk('local')->download($this->selectedFile->path);
+    }
+
+    public function toggleContextMenu(?int $x, ?int $y)
+    {
+        $this->x = $x ?? 0;
+        $this->y = $y ?? 0;
+        $this->contextMenu = !$this->contextMenu;
+    }
+
+    public function fileModal($id)
+    {
+        $this->selectedFile = Files::find($id);
+        Flux::modal('file-modal')->show();
     }
 
     public function render()
     {
-        Log::info($this->currentDirectory);
-        Log::info(auth()->user()->uuid);
 
         $directories = $this->currentDirectory->children ?? [];
         $files = $this->currentDirectory->files ?? [];
--- resources/views/livewire/file-manager.blade.php
+++ resources/views/livewire/file-manager.blade.php
@@ -1,5 +1,18 @@
-<div>
-    <div >
+<div class="h-full relative"
+     wire:keydown.ctrl.f.window.prevent="createDirectory"
+>
+    <div
+        x-data="{
+            mouseX: 0,
+            mouseY: 0
+        }"
+        @contextmenu.prevent="
+            mouseX = $event.clientX;
+            mouseY = $event.clientY;
+            $wire.toggleContextMenu(mouseX, mouseY)
+        "
+        class="h-full relative"
+    >
 
         {{-- Breadcrumbs --}}
         <flux:breadcrumbs>
@@ -13,9 +26,10 @@
         </flux:breadcrumbs>
 
         {{-- Subdirectories --}}
-        <div class="grid grid-cols-4 gap-4 mt-4">
+        <div class="flex flex-col gap-2 mt-4">
             @foreach ($directories as $directory)
-                <div class="p-4 bg-gray-100 rounded shadow cursor-pointer hover:bg-gray-200 dark:bg-gray-700 dark:hover:bg-gray-600"
+                <div wire:key="directory-{{$directory->id}}"
+                     class="p-1 bg-zinc-100 rounded shadow cursor-pointer hover:bg-zinc-200 dark:bg-zinc-700 dark:hover:bg-zinc-600"
                      wire:click="navigateToDirectory({{ $directory->id }})">
                     📁 {{ $directory->name }}
                 </div>
@@ -23,12 +37,92 @@
         </div>
 
         {{-- Files --}}
-        <div class="grid grid-cols-4 gap-4 mt-6">
+        <div class="flex flex-col gap-2 mt-4">
             @foreach ($files as $file)
-                <div class="p-4 bg-white rounded shadow">
-                    📄 {{ $file->name }}
+                <div wire:key="file-{{$file->id}}"
+                     wire:click="fileModal({{$file->id}})"
+                     class=" p-1 bg-zinc-100 rounded shadow cursor-pointer hover:bg-zinc-200 dark:bg-zinc-700 dark:hover:bg-zinc-600">
+                    📄 {{ $file->name .'.'. $file->extension }}
                 </div>
             @endforeach
         </div>
     </div>
+    @if($contextMenu)
+        <div
+            @contextmenu.prevent=""
+            x-on:contextmenu.prevent
+            wire:click.outside="toggleContextMenu(0, 0)"
+            class="absolute z-10 p-2 bg-white dark:bg-zinc-700 rounded shadow border border-zinc-200 dark:border-zinc-600
+             min-w-72 flex flex-col gap-2
+            "
+            style="top: {{ $y-65 }}px; left: {{ $x - (session('collapsed')? '80' : '310') }}px;"
+        >
+            <div class="flex flex-row justify-end items-center gap-2">
+                <flux:button square :loading="false" variant="subtle" icon="x" size="xs"
+                             wire:click="toggleContextMenu(0, 0)"
+                />
+            </div>
+            <div class="flex flex-col gap-2">
+                <flux:button
+                    icon="upload"
+                    variant="subtle"
+                    size="sm"
+                    :loading="false"
+                    wire:click="fileUploadModal"
+                    class="justify-start!"
+                >
+                    Upload File
+                </flux:button>
+                <flux:button
+                    icon="folder-plus"
+                    variant="subtle"
+                    size="sm"
+                    :loading="false"
+                    wire:click="createDirectory"
+                    class="justify-start!"
+                >
+                    New Folder
+                </flux:button>
+
+            </div>
+        </div>
+    @endif
+
+    <flux:modal name="file-modal" variant="flyout">
+        <div class="space-y-6">
+            <flux:heading>File Details</flux:heading>
+            <flux:separator/>
+            @if($selectedFile)
+                <div class="flex flex-col justify-center items-center gap-3.5">
+                    <div class="flex flex-col gap-3 items-center justify-center">
+                        <div class="h-48 w-48 rounded-lg">
+                            <img
+                                src="{{ Storage::disk('local')->temporaryUrl($selectedFile->path, now()->addMinutes(5)) }}"
+                                class="w-full h-full object-contain rounded-lg" alt="{{$selectedFile->alt}}">
+                        </div>
+                        <flux:heading level="2" size="lg">{{$selectedFile->name .'.'. $selectedFile->extension}}</flux:heading>
+                    </div>
+
+                    <div class="flex flex-row justify-self-end w-full mt-auto">
+                        <flux:spacer/>
+                        <flux:button icon="download" variant="primary" wire:click="download">Download</flux:button>
+                    </div>
+                </div>
+
+            @endif
+        </div>
+    </flux:modal>
+    <flux:modal name="upload-file" class="w-96!">
+        <div class="space-y-6">
+            <flux:heading>Upload File</flux:heading>
+            <flux:text variant="subtle">Maximum 10mb per file</flux:text>
+
+            <flux:input type="file" wire:model="upload" label="File"/>
+            <div class="flex flex-row justify-end">
+
+                <flux:button type="submit" variant="primary" wire:click="uploadFile">Upload</flux:button>
+            </div>
+        </div>
+    </flux:modal>
+
 </div>
