d435565eef8b737867c2dda5fb5bd37c001ad5cf
tomcr <tom.cremer@student.hepl.be>
2025-05-24 14:36:27 +0200

    Added - the different feature view

--- resources/views/livewire/projects/show.blade.php
+++ resources/views/livewire/projects/show.blade.php
@@ -2,16 +2,17 @@
 
     <flux:navbar>
         @foreach($features as $key => $feature)
-            <flux:navbar.item wire:click="changeView('{{$key}}')" icon="{{$feature['icon']}}" :current="($activeFeature === $key)" >{{ucfirst($feature['label'])}}</flux:navbar.item>
+            <flux:navbar.item wire:click="changeView('{{$key}}')" icon="{{$feature['icon']}}"
+                              :current="($activeFeature === $key)">{{ucfirst($feature['label'])}}</flux:navbar.item>
         @endforeach
     </flux:navbar>
     <div class="py-3 flex-1 overflow-hidden">
         @if($activeFeature === 'todos')
             <livewire:todo :projectId="$project->id"/>
         @elseif($activeFeature === 'files')
-            Files
+            <livewire:file-manager/>
         @elseif($activeFeature === 'insights')
-            Insights
+            <livewire:insights :projectId="$project->id"/>
         @elseif($activeFeature === 'settings')
             Settings
         @endif
