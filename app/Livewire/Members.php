d99b2978d9f6e43397c776d2846854f8d1c5ccea
tomcr <tom.cremer@student.hepl.be>
2025-05-24 14:22:09 +0200

    Added - Role edit

--- app/Livewire/Members.php
+++ app/Livewire/Members.php
@@ -4,32 +4,74 @@ namespace App\Livewire;
 
 use App\Helper\Context;
 use App\Livewire\Component\HorixtComponent;
-use App\Models\User;
 use Flux\Flux;
 use Livewire\Attributes\On;
+use Spatie\Permission\Models\Role;
 
 class Members extends HorixtComponent
 {
 
     private $members = [];
-    public $memberToEdit;
+    public $roles;
 
-    #[On('edit-member-done')]
-    public function loadMembers()
+    public $memberToEdit;
+    public $is_active = false;
+
+    public $search = '';
+    public $searchResults = [];
+
+    public function mount()
     {
-        $this->members = Context::getOrganization()->members;
+        $this->roles = Role::all();
     }
+
 
     public function editMember($id)
     {
-        $this->memberToEdit = User::find($id);
+        $this->memberToEdit = Context::getOrganization()->allMembers()->find($id);
+        $this->is_active = (bool)$this->memberToEdit->pivot->is_active;
         Flux::modal('edit-member')->show();
     }
 
+    public function searchRole()
+    {
+        $this->searchResults = $this->roles->filter(function ($role) {
+            return str_contains($role->name, strtolower($this->search));
+        });
+    }
+
+    public function removeRole($memberId, $roleId)
+    {
+        $member = Context::getOrganization()->allMembers()->find($memberId);
+        $member->removeRole($roleId);
+        $this->dispatch('toast', [
+            'title' => 'Role removed',
+            'message' => 'Role has been successfully removed',
+            'type' => 'success', // success, warning, error, info
+        ]);
+    }
+
+    public function addRole($memberId, $roleId)
+    {
+        $member = Context::getOrganization()->allMembers()->find($memberId);
+        $member->assignRole($roleId);
+        $this->dispatch('toast', [
+            'title' => 'Role added',
+            'message' => 'Role has been successfully added',
+            'type' => 'success', // success, warning, error, info
+        ]);
+    }
+
+
     public function render()
     {
-
-        $this->members = Context::getOrganization()->members;
+        if ($this->search !== '') {
+            $this->searchRole();
+        }
+        if ($this->search === '') {
+            $this->searchResults = [];
+        }
+        $this->members = Context::getOrganization()->members()->orderBy('joined_at', 'asc')->get();
         return view('livewire.members');
     }
 }
--- resources/views/livewire/members.blade.php
+++ resources/views/livewire/members.blade.php
@@ -1,12 +1,12 @@
-<div class="overflow-hidden font-lexend">
+<div class="font-lexend h-full flex flex-col ">
     <div class="flex justify-between items-center mb-4">
         <h2 class="text-xl font-semibold">Members</h2>
         <livewire:partials.add-members/>
     </div>
 
 
-    <div class="overflow-x-auto">
-        <table class="min-w-full shadow overflow-auto border border-zinc-300 dark:border-zinc-600 ">
+    <div class=" overflow-auto h-full">
+        <table class="min-w-full  shadow overflow-auto border border-zinc-300 dark:border-zinc-600 ">
             <thead class=" text-left text-sm font-medium text-gray-700">
             <tr class="border-b border-zinc-300 dark:border-zinc-600">
                 <th class="px-4 py-3 text-black dark:text-white  ">Name</th>
@@ -32,23 +32,117 @@
                                 </div>
                             @endif
 
-                            <span>{{ $member->name }}</span>
+                            <span class="whitespace-nowrap">{{ $member->name }}</span>
+
+                            @if ($member->isOwner(\App\Helper\Context::getOrganizationId()))
+                                <flux:badge color="green" size="sm">Owner</flux:badge>
+                            @endif
                         </div>
                     </td>
                     <td class="px-4 py-3 border-r border-zinc-300 dark:border-zinc-600">
                         {{ $member->email }}
                     </td>
                     <td class="px-4 py-3 border-r border-zinc-300 dark:border-zinc-600">
-                        <div class="flex items-center gap-2">
-                            @if ($member->isOwner(\App\Helper\Context::getOrganizationId()))
-                                <flux:badge color="green" size="sm">Owner</flux:badge>
-                            @endif
-                            @forelse($member->roles as $role)
-                                <flux:badge color="{{\App\Enums\RoleEnum::from($role->name)->color()}}"
-                                            size="sm">{{ ucfirst($role->name) }}</flux:badge>
-                            @empty
-                            @endforelse
+
+                        <div x-data="{ memberRoles: false }"
+
+                             class="relative w-full h-full
+                            grid grid-cols-[1fr_auto] gap-1.5 items-center"
+                        >
+                            <div class="flex flex-wrap gap-1.5">
+                                @forelse($member->roles as $role)
+                                    <flux:badge color="{{\App\Enums\RoleEnum::from($role->name)->color()}}"
+                                                size="sm">{{ ucfirst($role->name) }}</flux:badge>
+                                @empty
+                                    <flux:text>No role</flux:text>
+                                @endforelse
+                            </div>
+                            @if( auth()->user()->can(\App\Enums\PermissionEnum::ADMIN_MANAGE->value))
+
+                                <flux:button square icon="user-round-cog" size="xs" variant="subtle"
+                                             x-on:click="memberRoles = true" class="cursor-pointer"
+                                />
+                                <!--Modal-->
+                                <div x-show="memberRoles" x-on:click.away="memberRoles = false"
+                                     class="absolute bg-white dark:bg-zinc-700 top-10 right-0 rounded-lg shadow-md border border-zinc-200 dark:border-zinc-500
+                                 grid grid-cols-1 grid-rows-[repeat(4,minmax(auto,1fr)] gap-1.5 p-2.5 min-w-64 max-w-72 min-h-56  z-50"
+                                >
+                                    <div>
+                                        <flux:input
+                                            wire:model.live="search"
+                                            wire:keydown.enter.prevent="searchRole"
+                                            icon="magnifying-glass"
+                                            placeholder="Search"
+                                            clearable
+                                            size="sm"
+                                        />
+
+                                        <div class="overflow-y-auto max-h-36 my-2">
+                                            <div class="flex flex-wrap gap-1.5 mt-2">
+                                                @foreach(collect($searchResults)->reject(
+                                                    fn($result) => $member->roles->contains('id', $result->id)
+                                                    ) as $result)
+                                                    <flux:badge
+                                                        color="{{\App\Enums\RoleEnum::from($result->name)->color()}}"
+                                                        size="sm">{{ ucfirst($result->name) }}</flux:badge>
+                                                @endforeach
+                                            </div>
+                                        </div>
+
+                                        <flux:separator/>
+                                    </div>
+                                    <div class="flex flex-col gap-2.5">
+                                        <div class="flex flex-col">
+                                            <flux:heading level="3" size="lg">
+                                                Assigned Roles
+                                            </flux:heading>
+                                            <flux:text variant="subtle" class="text-[0.65rem]">
+                                                At least one role need to be applied.
+                                            </flux:text>
+                                        </div>
+                                        <div class="flex flex-wrap max-h-32 overflow-y-auto gap-1">
+                                            @forelse($member->roles as $role)
+                                                <flux:badge color="{{\App\Enums\RoleEnum::from($role->name)->color()}}"
+                                                            size="sm"
+                                                            class="h-fit w-fit">
+                                                    {{ ucfirst($role->name) }}
+                                                    @if($member->roles->count() > 1)
+                                                        <flux:badge.close
+                                                            wire:click="removeRole({{ $member->id }}, {{ $role->id }})"/>
+                                                    @endif
+                                                </flux:badge>
+                                            @empty
+                                                <flux:text>No role</flux:text>
+                                            @endforelse
+                                        </div>
+                                    </div>
+                                    <flux:separator/>
+                                    <div class="flex flex-col gap-2.5">
+                                        <div class="flex flex-col">
+                                            <flux:heading level="3" size="lg">
+                                                Roles
+                                            </flux:heading>
+                                            <flux:text variant="subtle" class="text-[0.65rem]">
+                                                Click on a role to add it.
+                                            </flux:text>
+                                        </div>
+                                        <div class="flex flex-wrap max-h-32 overflow-y-auto gap-1 h-fit">
+                                            @foreach($roles->reject(
+                                                fn($role) => $member->roles->contains('id', $role->id)
+                                            ) as $role)
+                                                <flux:badge color="{{\App\Enums\RoleEnum::from($role->name)->color()}}"
+                                                            size="sm"
+                                                            class="h-fit w-fit pointer-events-auto cursor-pointer"
+                                                            wire:click="addRole({{ $member->id }}, {{ $role->id }})">
+                                                    {{ ucfirst($role->name) }}
+                                                </flux:badge>
+                                            @endforeach
+                                        </div>
+                                    </div>
+                                </div>
+
                         </div>
+                        @endif
                     </td>
 
                     <td class="px-4 py-3 border-r border-zinc-300 dark:border-zinc-600">
@@ -66,7 +160,7 @@
                                     variant="primary" size="sm">
                                     Edit
                                 </flux:button>
-                                @endcan
+                            @endcan
                         </div>
                     </td>
                 </tr>
@@ -81,10 +175,29 @@
         </table>
     </div>
 
-    <flux:modal name="edit-member">
-        <div>
-            hola
-            {{$memberToEdit}}
+    <flux:modal name="edit-member" class=" w-full max-w-96">
+        <div class="flex flex-col gap-4">
+            <div class="flex flex-col gap-2">
+                <flux:heading size="lg" level="3">Edit Member</flux:heading>
+                <flux:text>Here you can edit a member.</flux:text>
+            </div>
+
+            <div class="flex flex-col gap-4">
+                <div class="flex flex-col gap-2">
+                    <flux:label for="role">Member Roles</flux:label>
+                    <div id="role" class="flex flex-wrap gap-2">
+                        @foreach($memberToEdit->roles ?? [] as $memberRole)
+                            <flux:badge color="{{\App\Enums\RoleEnum::from($memberRole->name)->color()}}"
+                                        size="sm">
+                                {{ ucfirst($memberRole->name) }}
+                                <flux:badge.close/>
+                            </flux:badge>
+                        @endforeach
+                    </div>
+                </div>
+
+
+            </div>
         </div>
     </flux:modal>
 
