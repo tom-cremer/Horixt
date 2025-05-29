2c8108d6a6ea174d095b5060e26b02754108e1e9
tomcr <tom.cremer@student.hepl.be>
2025-05-30 00:23:45 +0200

    Improvements - Using the flux ui to be uniform + using the user's avatar

--- resources/views/livewire/partials/team-bubble.blade.php
+++ resources/views/livewire/partials/team-bubble.blade.php
@@ -1,32 +1,27 @@
 <div class="flex items-center justify-center ">
 
     @if($mode === 'members')
-
-        @foreach($members->take(5) as $member)
-            <flux:tooltip content="{{$member->name}}" position="bottom">
-                <div
-                    class="peer w-8 h-8 rounded-full border-1 bg-neutral-200 dark:bg-zinc-600  border-white flex items-center justify-center -m-0.5">
-                    <span class="text-xs font-bold font-lexend ">
-                        {{$member->initials()}}
-                    </span>
-                </div>
-            </flux:tooltip>
-        @endforeach
-        @if(count($members) >= 5)
-            <div class="w-8 h-8 rounded-full border-1 border-white flex items-center justify-center bg-gray-200">
-                <span class="text-xs font-bold font-lexend">
-                    +{{ count($members) - 5 }}
-                </span>
-            </div>
-        @endif
+        <flux:avatar.group class="ring-0! ring-transparent!">
+            @foreach($members->take(5) as $member)
+                @if($member->avatar)
+                    <flux:avatar tooltip="{{$member->name}}" size="sm" class="ring-0! ring-transparent!"
+                                 src="{{\Illuminate\Support\Facades\Storage::url($member->avatar->path)}}"/>
+                @else
+                    <flux:avatar tooltip="{{$member->name}}" size="sm" name="{{$member->name}}" class="ring-0! ring-transparent!"/>
+                @endif
+            @endforeach
+            @if(count($members) >= 5)
+                <flux:avatar>{{count($members) - 5}}+</flux:avatar>
+            @endif
+        </flux:avatar.group>
 
     @else
-        <flux:tooltip content="{{$owner->name}}" position="bottom">
-            <div class="w-8 h-8 rounded-full border-1 bg-neutral-200 dark:bg-zinc-600 border-white flex items-center justify-center ">
-                <span class="text-xs font-bold font-lexend">
-                    {{$owner->initials()}}
-                </span>
-            </div>
-        </flux:tooltip>
+        @if($owner->avatar)
+            <flux:avatar tooltip="{{$owner->name}}" size="sm"
+                         src="{{\Illuminate\Support\Facades\Storage::url($owner->avatar->path)}}"/>
+        @else
+            <flux:avatar tooltip="{{$owner->name}}" size="sm" name="{{$owner->name}}"/>
+        @endif
+
     @endif
 </div>
