d4e7b5372b32cde56948901ad0fbad028a0707b2
tomcr <tom.cremer@student.hepl.be>
2025-05-30 00:06:02 +0200

    Added - Trackable ability

--- database/migrations/0001_01_01_000008_create_todos_table.php
+++ database/migrations/0001_01_01_000008_create_todos_table.php
@@ -15,6 +15,7 @@ return new class extends Migration {
             $table->string('name');
             $table->text('description');
             $table->boolean('is_done')->default(false);
+            $table->boolean('is_trackable')->default(true);
             $table->unsignedBigInteger('status_id');
             $table->unsignedBigInteger('priority_id');
             $table->unsignedBigInteger('color_id');
