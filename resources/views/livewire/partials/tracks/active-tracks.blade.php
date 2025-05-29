d0ebe993329f89ed715a6701085e695fccb53d7d
tomcr <tom.cremer@student.hepl.be>
2025-05-30 00:27:15 +0200

    Slight changes

--- resources/views/livewire/partials/tracks/active-tracks.blade.php
+++ resources/views/livewire/partials/tracks/active-tracks.blade.php
@@ -1,9 +1,17 @@
+@php
+    $activeTracksMessage = match ($activeTracks->count()) {
+        0 => 'No active tracks',
+        1 => '1 Track\'s running',
+        default => $activeTracks->count() . ' Tracks are running',
+    };
+@endphp
+
+
 <div class="relative">
     <flux:tooltip
-        content="{{!($activeTracks->count() > 0) ? 'No active tracks': $activeTracks->count() .' Tracks running'}}">
-        {{--
-                <flux:button :loading="false" square size="sm" variant="ghost" wire:click="toggle">
-        --}}
+        content="{{ $activeTracksMessage }}"
+    >
+
         <span class="relative flex size-3">
             @if(($activeTracks->count() > 0))
                 <span
@@ -12,26 +20,7 @@
           <span
               class="relative inline-flex size-3 rounded-full {{!($activeTracks->count() > 0)? 'bg-zinc-500' : 'bg-green-500'}}"></span>
         </span>
-        {{--
-                </flux:button>
-        --}}
     </flux:tooltip>
-
-    {{--@if($expanded || $activeTracks->count() > 0)
-        <div
-            class="absolute bg-neutral-50 border border-zinc-200 dark:border-zinc-500 dark:bg-zinc-700 top-100 right-0 rounded shadow-md p-4">
-            @if($activeTracks->count() > 0)
-                <ul>
-                    @foreach($activeTracks as $track)
-                        <flux:text>{{ $track->todo->name }}</flux:text>
-                        <livewire:track :todo="$track->todo->id" :key="'active-track-'.$track->id"/>
-                    @endforeach
-                </ul>
-            @else
-                <p class="text-gray-500 dark:text-gray-400">No active tracks available.</p>
-            @endif
-        </div>
-    @endif--}}
 </div>
 
 
