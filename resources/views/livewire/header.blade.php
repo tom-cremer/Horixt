b4443458750b83d9a7dd34a88b52e3dac90b6b0c
tomcr <tom.cremer@student.hepl.be>
2025-06-02 22:03:48 +0200

    Moved the appearance dropdown in the header instead of the sidebar

--- resources/views/livewire/header.blade.php
+++ resources/views/livewire/header.blade.php
@@ -1,13 +1,33 @@
 <div>
-    <flux:header class="flex justify-end gap-5 px-0!">
+    <flux:header class="flex justify-end gap-3.5 px-0!">
         <livewire:partials.breadcrumb/>
         <div class="max-w-[500px] w-full">
             <flux:input
+                size="sm"
                 wire:model="search"
                 type="text"
                 placeholder="{{ __('Search') }}"
             />
         </div>
-        <livewire:partials.tracks.active-tracks/>
+        <div class="flex items-center gap-2">
+            <flux:dropdown x-data align="end" position="bottom">
+                <flux:button variant="subtle" square class="group" size="sm" aria-label="Preferred color scheme">
+                    <flux:icon.sun x-show="$flux.appearance === 'light'" variant="mini"
+                                   class="text-zinc-500 dark:text-white"/>
+                    <flux:icon.moon x-show="$flux.appearance === 'dark'" variant="mini"
+                                    class="text-zinc-500 dark:text-white"/>
+                    <flux:icon.moon x-show="$flux.appearance === 'system' && $flux.dark" variant="mini"/>
+                    <flux:icon.sun x-show="$flux.appearance === 'system' && ! $flux.dark" variant="mini"/>
+                </flux:button>
+
+                <flux:menu>
+                    <flux:menu.item icon="sun" x-on:click="$flux.appearance = 'light'">Light</flux:menu.item>
+                    <flux:menu.item icon="moon" x-on:click="$flux.appearance = 'dark'">Dark</flux:menu.item>
+                    <flux:menu.item icon="computer-desktop" x-on:click="$flux.appearance = 'system'">System
+                    </flux:menu.item>
+                </flux:menu>
+            </flux:dropdown>
+            <livewire:partials.tracks.active-tracks/>
+        </div>
     </flux:header>
 </div>
