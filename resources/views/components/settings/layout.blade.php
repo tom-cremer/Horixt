fc9d82ed67cdb77c483ae5a381e56280c19f5aa5
tomcr <tom.cremer@student.hepl.be>
2025-06-09 20:57:10 +0200

    Improvements - Settings layout rework

--- resources/views/components/settings/layout.blade.php
+++ resources/views/components/settings/layout.blade.php
@@ -1,32 +1,10 @@
-<div class="flex items-start max-md:flex-col">
-    <div class="mr-10 w-full pb-4 md:w-[220px]">
-        <flux:navlist>
-            <flux:navlist.item
-                href="{{(\App\Helper\Context::isOrganization())? route('organization.settings.profile', ['slug' => \App\Helper\Context::getOrganizationSlug()]) : route('personal.settings.profile')}}"
-                wire:navigate>
-                {{ __('Profile') }}
-            </flux:navlist.item>
-            <flux:navlist.item
-                href="{{(\App\Helper\Context::isOrganization())? route('organization.settings.password', ['slug' => \App\Helper\Context::getOrganizationSlug()]) : route('personal.settings.password')}}"
-                wire:navigate>
-                {{ __('Password') }}
-            </flux:navlist.item>
-            <flux:navlist.item
-                href="{{(\App\Helper\Context::isOrganization())? route('organization.settings.appearance', ['slug' => \App\Helper\Context::getOrganizationSlug()]) : route('personal.settings.appearance')}}"
-                wire:navigate>
-                {{ __('Appearance') }}
-            </flux:navlist.item>
-        </flux:navlist>
+<div class="flex flex-col items-start w-full">
+    <div class="flex flex-col mb-4 w-full">
+     <flux:heading size="lg" >{{ $heading ?? '' }}</flux:heading>
+        <flux:subheading>{{ $subheading ?? '' }}</flux:subheading>
     </div>
 
-    <flux:separator class="md:hidden"/>
-
-    <div class="flex-1 self-stretch max-md:pt-6">
-        <flux:heading>{{ $heading ?? '' }}</flux:heading>
-        <flux:subheading>{{ $subheading ?? '' }}</flux:subheading>
-
-        <div class="mt-5 w-full max-w-lg">
+        <div class="w-full">
             {{ $slot }}
         </div>
-    </div>
 </div>
