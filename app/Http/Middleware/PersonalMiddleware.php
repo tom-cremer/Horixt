d5fee4df20045f2e0662f219b34a27919013c770
tomcr <tom.cremer@student.hepl.be>
2025-06-09 20:59:08 +0200

    Cleaning - removed logs

--- app/Http/Middleware/PersonalMiddleware.php
+++ app/Http/Middleware/PersonalMiddleware.php
@@ -3,16 +3,13 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
-use Symfony\Component\HttpFoundation\Response;
 
 class PersonalMiddleware
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
      */
     public function handle($request, Closure $next)
     {
@@ -20,7 +17,6 @@ class PersonalMiddleware
         if (!auth()->check()) {
             return redirect()->route('login');
         }
-        Log::info("PersonalMiddleware: User is authenticated");
         // Clear any active org session
         session()->forget(['organization_id', 'team_id']);
         return $next($request);
