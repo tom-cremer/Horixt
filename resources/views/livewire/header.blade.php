4008c09efabe20982eea1092227000c5fe478b1e
tomcr <tom.cremer@student.hepl.be>
2025-06-09 20:56:10 +0200

    Responsive

--- resources/views/livewire/header.blade.php
+++ resources/views/livewire/header.blade.php
@@ -3,7 +3,7 @@
         <livewire:partials.breadcrumb/>
         <livewire:partials.search/>
         <div class="flex items-center gap-2">
-            <flux:dropdown x-data align="end" position="bottom">
+            <flux:dropdown x-data align="end" position="bottom" class="hidden sm:block">
                 <flux:button variant="subtle" square class="group" size="sm" aria-label="Preferred color scheme">
                     <flux:icon.sun x-show="$flux.appearance === 'light'" variant="mini"
                                    class="text-zinc-500 dark:text-white"/>
