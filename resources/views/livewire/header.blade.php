3583e7a119eb0794368cd2dff8031a20fe3ff453
tomcr <tom.cremer@student.hepl.be>
2025-06-12 00:08:41 +0200

    Changed the dropdown into a lighter version

--- resources/views/livewire/header.blade.php
+++ resources/views/livewire/header.blade.php
@@ -3,24 +3,11 @@
         <livewire:partials.breadcrumb/>
         <livewire:partials.search/>
         <div class="flex items-center gap-2">
-        <livewire:partials.notifications/>
-            <flux:dropdown x-data align="end" position="bottom" class="hidden sm:block">
-                <flux:button variant="subtle" square class="group" size="sm" aria-label="Preferred color scheme">
-                    <flux:icon.sun x-show="$flux.appearance === 'light'" variant="mini"
-                                   class="text-zinc-500 dark:text-white"/>
-                    <flux:icon.moon x-show="$flux.appearance === 'dark'" variant="mini"
-                                    class="text-zinc-500 dark:text-white"/>
-                    <flux:icon.moon x-show="$flux.appearance === 'system' && $flux.dark" variant="mini"/>
-                    <flux:icon.sun x-show="$flux.appearance === 'system' && ! $flux.dark" variant="mini"/>
-                </flux:button>
-
-                <flux:menu>
-                    <flux:menu.item icon="sun" x-on:click="$flux.appearance = 'light'">Light</flux:menu.item>
-                    <flux:menu.item icon="moon" x-on:click="$flux.appearance = 'dark'">Dark</flux:menu.item>
-                    <flux:menu.item icon="computer-desktop" x-on:click="$flux.appearance = 'system'">System
-                    </flux:menu.item>
-                </flux:menu>
-            </flux:dropdown>
+            <livewire:partials.notifications/>
+            <div>
+                <flux:button x-data size="sm" x-on:click="$flux.dark = ! $flux.dark" icon="moon" variant="subtle"
+                             aria-label="Toggle dark mode"/>
+            </div>
             <livewire:partials.tracks.active-tracks/>
         </div>
     </flux:header>
