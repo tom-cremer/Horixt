7d62626ef14aa217a6c618ed2aeed5b1fef4f5cf
tomcr <tom.cremer@student.hepl.be>
2025-06-12 00:14:04 +0200

    Avatar update

--- resources/views/livewire/sidebar.blade.php
+++ resources/views/livewire/sidebar.blade.php
@@ -206,12 +206,18 @@
             <flux:menu.radio.group>
                 <div class="p-0 text-sm font-normal">
                     <div class="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                        <span class="relative flex h-8 w-8 shrink-0 overflow-hidden rounded-lg">
-                            <span
-                                class="flex h-full w-full items-center justify-center rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
-                                {{ auth()->user()->initials() }}
-                            </span>
-                        </span>
+                        @if(auth()->user()->avatar)
+                            <flux:avatar tooltip="{{auth()->user()->name}}" size="sm"
+                                         class="ring-0! ring-transparent!"
+                                         src="{{\Illuminate\Support\Facades\Storage::url(auth()->user()->avatar->path)}}"/>
+                        @else
+                            <flux:avatar tooltip="{{auth()->user()->name}}" size="sm"
+                                         name="{{auth()->user()->name}}"
+                                         class="ring-0! ring-transparent!²"
+                                         color="auto"
+                                         color:seed="{{ auth()->user()->id }}"
+                                         initials:single/>
+                        @endif
 
                         <div class="grid flex-1 text-left text-sm leading-tight">
                             <span class="truncate font-semibold">{{ auth()->user()->name }}</span>
