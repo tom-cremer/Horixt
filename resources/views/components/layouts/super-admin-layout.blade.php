4eb2b40ae85bc4e5365baa0b9db4e820b259bf53
tomcr <tom.cremer@student.hepl.be>
2025-06-19 00:55:09 +0200

    Super admin layout

--- resources/views/components/layouts/super-admin-layout.blade.php
+++ resources/views/components/layouts/super-admin-layout.blade.php
@@ -3,16 +3,17 @@
 <head>
     @include('partials.head')
 </head>
-<body class="min-h-screen bg-white dark:bg-zinc-800 grid grid-cols-[auto_1fr] grid-rows-1" style="overflow-x: hidden;">
+<body class="min-h-screen bg-white dark:bg-zinc-800 grid grid-cols-1 grid-rows-1" style="overflow-x: hidden;">
 {{--Admin sidebar--}}
 {{--@livewire('sidebar')--}}
-<main class="p-5 pt-0 h-screen flex flex-col w-full overflow-hidden">
+<main class="h-screen flex flex-col w-full overflow-hidden">
     {{--Admin header--}}
     {{--@livewire('header')--}}
-    <div class="flex flex-col gap-4 mt-4 overflow-y-auto row-start-2 row-end-3">
+    <div class="flex flex-col gap-4 overflow-y-auto row-start-2 row-end-3">
         {{ $slot }}
     </div>
 </main>
+<livewire:partials.feedback.toast/>
 @fluxScripts
 </body>
 </html>
