d98ddaacd1f7377611367b81ecb8cd6bbe965a4e
tomcr <tom.cremer@student.hepl.be>
2025-06-19 00:52:03 +0200

    Fix - component was overflowing when changing colors

--- resources/views/livewire/settings/preferences.blade.php
+++ resources/views/livewire/settings/preferences.blade.php
@@ -80,82 +80,85 @@
 
             {{--Status and Priority Colors--}}
             <div class="flex flex-col gap-4">
-            <flux:heading level="3" size="lg">Colors Personalization</flux:heading>
-            <div class="grid grid-cols-1 sm:grid-cols-[1fr_auto_1fr] max-w-2xl gap-6">
-                {{--Status color--}}
-                <div class="flex flex-col gap-3">
-                    <flux:heading level="3">Statuses</flux:heading>
-                    <div
-                        class="flex flex-col gap-2">
-                        @foreach($statuses as $status)
-                            <div x-data="{ editStatus: false }"
-                                 class="relative grid grid-cols-[1fr_auto] gap-2">
-                                <flux:badge :color="$status->userStatusColor->color->alias"
-                                            class="w-fit">{{ $status->name }} </flux:badge>
-                                <div class="relative">
+                <flux:heading level="3" size="lg">Colors Personalization</flux:heading>
+                <div class="grid grid-cols-1 sm:grid-cols-[1fr_auto_1fr] max-w-2xl gap-6 h-full">
+                    {{--Status color--}}
+                    <div class="flex flex-col gap-3">
+                        <flux:heading level="3">Statuses</flux:heading>
+                        <div
+                            class="flex flex-col gap-2">
+                            @foreach($statuses as $status)
+                                <div x-data="{ editStatus: false }"
+                                     class="relative grid grid-cols-[1fr_auto] gap-2">
+                                    <flux:badge :color="$status->userStatusColor->color->alias"
+                                                class="w-fit">{{ $status->name }} </flux:badge>
+                                    <div class="relative">
 
-                                    <button x-on:click="editStatus = !editStatus"
-                                            class="mr-1.5 p-0.5 w-8 h-6 rounded-md border border-zinc-300 dark:border-zinc-600
+                                        <button x-on:click="editStatus = !editStatus"
+                                                class="mr-1.5 p-0.5 w-8 h-6 rounded-md border border-zinc-300 dark:border-zinc-600
                                             focus:outline-none focus:ring-2 focus:ring-zinc-800 dark:focus:ring-zinc-100">
                                 <span
                                     class="block {{$status->userStatusColor->color->alias}} w-full h-full rounded-sm"> </span>
-                                    </button>
-                                    <div x-show="editStatus"
-                                         @click.away="editStatus = false"
-                                         class="absolute top-full right-full grid grid-cols-4 items-center justify-center gap-2 bg-zinc-100 dark:bg-zinc-700 p-2 rounded-md z-10 shadow-md min-w-32">
-                                        @foreach($colors as $color)
-                                            <button wire:click="updateStatusColor({{ $status->id }}, {{ $color->id }})"
+                                        </button>
+                                        <div x-show="editStatus"
+                                             @click.away="editStatus = false"
+                                             class="absolute top-full right-full grid grid-cols-4 items-center justify-center gap-2 bg-zinc-100 dark:bg-zinc-700 p-2 rounded-md z-10 shadow-md min-w-32">
+                                            @foreach($colors as $color)
+                                                <button
+                                                    wire:click="updateStatusColor({{ $status->id }}, {{ $color->id }})"
                                                     class="w-6 h-6 rounded-full {{ $color->alias }} focus:ring-2 ring-offset-2 ring-zinc-500"
                                                     title="{{ $color->name }}">
-                                            </button>
-                                        @endforeach
+                                                </button>
+                                            @endforeach
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        @endforeach
+                            @endforeach
+                        </div>
                     </div>
-                </div>
 
-                <flux:separator vertical class="hidden sm:block"/>
-                <flux:separator class="block sm:hidden"/>
-                {{--Priority color--}}
-                <div class="flex flex-col gap-3">
-                    <flux:heading level="3">Priorities</flux:heading>
-                    <div
-                        class="flex flex-col gap-2">
-                        @foreach($priorities as $priority)
+                    <flux:separator vertical class="hidden sm:block"/>
+                    <flux:separator class="block sm:hidden"/>
+                    {{--Priority color--}}
+                    <div class="flex flex-col gap-3">
+                        <flux:heading level="3">Priorities</flux:heading>
+                        <div
+                            class="flex flex-col gap-2">
+                            @foreach($priorities as $priority)
 
-                            <div x-data="{ editPriority: false }"
-                                 class="relative grid grid-cols-[1fr_auto] gap-2">
-                                <flux:badge :color="$priority->userPriorityColor->color->alias"
-                                            class="w-fit">{{ $priority->name }} </flux:badge>
-                                <div class="relative">
+                                <div x-data="{ editPriority: false }"
+                                     class="relative grid grid-cols-[1fr_auto] gap-2">
+                                    <flux:badge :color="$priority->userPriorityColor->color->alias"
+                                                class="w-fit">{{ $priority->name }} </flux:badge>
+                                    <div class="relative">
 
-                                    <button x-on:click="editPriority = !editPriority"
-                                            class="mr-1.5 p-0.5 w-8 h-6 rounded-md border border-zinc-300 dark:border-zinc-600
+                                        <button x-on:click="editPriority = !editPriority"
+                                                class="mr-1.5 p-0.5 w-8 h-6 rounded-md border border-zinc-300 dark:border-zinc-600
                                     focus:outline-none focus:ring-2 focus:ring-zinc-800 dark:focus:ring-zinc-100">
                                     <span
                                         class="block {{$priority->userPriorityColor->color->alias}} w-full h-full rounded-sm"> </span>
-                                    </button>
-                                    <div x-show="editPriority"
-                                         @click.away="editPriority = false"
-                                         class="absolute top-full right-full grid grid-cols-4 items-center justify-center gap-2 bg-zinc-100 dark:bg-zinc-700 p-2 rounded-md z-10 shadow-md min-w-32">
-                                        @foreach($colors as $color)
-                                            <button
-                                                wire:click="updatePriorityColor({{ $priority->id }}, {{ $color->id }})"
-                                                class="w-6 h-6 rounded-full {{ $color->alias }} focus:ring-2 ring-offset-2 ring-zinc-500"
-                                                title="{{ $color->name }}">
-                                            </button>
-                                        @endforeach
+                                        </button>
+                                        <div x-show="editPriority"
+                                             @click.away="editPriority = false"
+                                             class="absolute top-full right-full grid grid-cols-4 items-center justify-center gap-2 bg-zinc-100 dark:bg-zinc-700 p-2 rounded-md z-10 shadow-md min-w-32">
+                                            @foreach($colors as $color)
+                                                <button
+                                                    wire:click="updatePriorityColor({{ $priority->id }}, {{ $color->id }})"
+                                                    class="w-6 h-6 rounded-full {{ $color->alias }} focus:ring-2 ring-offset-2 ring-zinc-500"
+                                                    title="{{ $color->name }}">
+                                                </button>
+                                            @endforeach
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        @endforeach
+                            @endforeach
+                        </div>
                     </div>
                 </div>
-            </div>
 
-        </div>
+            </div>
+            <div class="min-h-40"></div>
+
         </div>
     </x-settings.layout>
 </div>
